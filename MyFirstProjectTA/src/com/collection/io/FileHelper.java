1f8d97083791ce78511f77c315f781a0ac3b799a
Anna Kadykalo <akady@softserveinc.com>
2021-02-22 23:58:21 +0200

    Added ThreeSet example

--- MyFirstProjectTA/src/com/collection/io/FileHelper.java
+++ MyFirstProjectTA/src/com/collection/io/FileHelper.java
@@ -4,10 +4,12 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class FileHelper {
     private  ArrayList<String> list = new ArrayList<String>();
+//    private TreeSet<String> list = new TreeSet<String>();
 
     public  void readFile(String path ){
         try (BufferedReader bufferReader = new BufferedReader(new FileReader(path))) {
@@ -25,10 +27,15 @@ public class FileHelper {
         }
 
     }
-    public void sortNames(){
+    public void sortNamesArrayList(){
         list.sort(Collections.reverseOrder());
         System.out.println("After sorting: " + list.toString());
     }
+
+//    public void sortNamesThreeSet(){
+//        list = (TreeSet<String>) list.descendingSet();
+//        System.out.println("After sorting: " + list.toString());
+//    }
 
     public void writeFile() {
         try (FileWriter writer = new FileWriter("newfile.txt")) {
--- MyFirstProjectTA/src/com/collection/io/Main.java
+++ MyFirstProjectTA/src/com/collection/io/Main.java
@@ -1,10 +1,13 @@
 package com.collection.io;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main (String[] args){
         FileHelper students = new FileHelper();
         students.readFile("/Users/akady/IdeaProjects/TestAutomationCourse/MyFirstProjectTA/res/names.txt");
-        students.sortNames();
+        students.sortNamesArrayList();
+//        students.sortNamesThreeSet();
         students.writeFile();
     }
 }
