7900eada55c51872f3327a976b5e01f631783f2a
Anna Kadykalo <akady@softserveinc.com>
2021-03-03 23:48:50 +0200

    Test commit for branching

--- MyFirstProjectTA/src/com/pattern/Main.java
+++ MyFirstProjectTA/src/com/pattern/Main.java
@@ -2,32 +2,42 @@ package com.pattern;
 
 public class Main {
     public static void main (String[] args) {
-        Soldier.Builder builder =  new Soldier.Builder()
-                .setBadgeNumber(123)
-                .setBloodType("1-")
-                .setName("Jane")
-                .setSex("woman")
-                .setRank("officer");
-
-        Soldier jane = builder.build();
-        Soldier daria = builder
-                        .setName("Daria")
-                        .build();
-
+//        public static void main (String[] args) {
+//            Soldier.Builder builder =  new Soldier.Builder()
+//                    .setBadgeNumber(123)
+//                    .setBloodType("1-")
+//                    .setName("Jane")
+//                    .setSex("woman")
+//                    .setRank("officer");
+//
+//            Soldier jane = builder.build();
+//            Soldier daria = builder
+//                    .setName("Daria")
+//                    .build();
+////
 //        System.out.println("Soldier " + jane.getName() + '\t' + jane.getRank());
 //        System.out.println("Soldier " + daria.getName() + '\t' + daria.getRank());
 //        System.out.println("Soldier " + jane.getName() + '\t' + jane.getRank());
 
-        Triangle triangle1 = Triangle.getInstance();
+//        Triangle triangle1 = Triangle.getInstance();
+//
+//        triangle1.setA(3);
+//        triangle1.setB(4);
+//        triangle1.setC(5);
+//
+//        System.out.println("Perimeter 1: " + triangle1.calculatePerimeter());
+//
+//        Triangle triangle2 = Triangle.getInstance();
+//
+//        System.out.println("Perimeter 2: " + triangle1.calculatePerimeter());
 
-        triangle1.setA(3);
-        triangle1.setB(4);
-        triangle1.setC(5);
+        TestBuilder student = TestBuilder.NewBuilder.newInstance()
+                                .setId(1)
+                                .setName("Ivan")
+                                .build();
 
-        System.out.println("Perimeter 1: " + triangle1.calculatePerimeter());
+        System.out.println(student.getName());
 
-        Triangle triangle2 = Triangle.getInstance();
 
-        System.out.println("Perimeter 2: " + triangle1.calculatePerimeter());
     }
 }
