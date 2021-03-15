3a3e795f0d689fb6517e778586e0a731157b0093
Anna Kadykalo <akady@softserveinc.com>
2021-03-16 01:17:26 +0200

    Added parameters from xml

--- SeleniumTest/src/test/java/TestWithParameters.java
+++ SeleniumTest/src/test/java/TestWithParameters.java
@@ -1,4 +1,5 @@
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /*
@@ -6,11 +7,19 @@ import org.testng.annotations.Test;
 * parameters from testng.xml
 * parameters from DataProvider
 */
+
+
 public class TestWithParameters {
 
     @Test(dataProvider = "getNames")
     public void validateUsernameTest(String s){
         System.out.println("Data provider: " + s);
+    }
+
+    @Parameters("p1")
+    @Test
+    public void validateMailTest(String s){
+        System.out.println("XML: " + s);
     }
 
     @DataProvider
